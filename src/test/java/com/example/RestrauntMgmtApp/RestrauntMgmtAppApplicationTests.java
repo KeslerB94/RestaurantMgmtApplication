ad691743660612e03d9a36498318950cd1083a8a
Beau Kesler <keslerbeau94@outlook.com>
2024-12-20 21:48:00 -0800

    first commit

---
+++ src/test/java/com/example/RestrauntMgmtApp/RestrauntMgmtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestrauntMgmtApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrauntMgmtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
